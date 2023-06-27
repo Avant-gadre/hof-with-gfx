bfe1cb9109ce03df340a6859cc851a0ca1ce35c8
cupsmoke <42766513+cupsmoke@users.noreply.github.com>
2023-06-28 02:10:50 +0800

    123

--- tool/ALL.bat
+++ tool/ALL.bat
@@ -1,6 +1,6 @@
 call cd /d E:\download\github download\hof-with-gfx\tool
 
-call python "eventpic_gfxmaker.py"
+@REM call python "eventpic_gfxmaker.py"
 call python "from png to gfx-advisor.py"
 call python "from png to gfx-event.py"
 call python "from png to gfx-focus.py"
