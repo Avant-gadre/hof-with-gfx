24b178637011e5c43a25d6931fc04bbec2909525
Avant-gadre <49426761+Avant-gadre@users.noreply.github.com>
2023-11-04 18:16:34 +0800

    State compare

---
+++ tool/state compare/test.py
@@ -0,0 +1,93 @@
+import re
+import os
+import io
+import os.path
+
+# 根据文件夹，获得其中所有文件的文件名
+def getDir(path):
+    taglist = []
+    taglist1 = os.listdir(path)
+    for tag in taglist1:
+        path1 = path + tag
+        if os.path.isdir(path1):
+            taglist.append(tag)
+    return taglist
+
+def getTxtName(path):
+    # 获取指定路径内所有txt文件名
+    list0 = os.listdir(path)
+    listout = []
+    for name in list0:
+        if '.txt' in name:
+            listout.append(name)
+    return listout
+# print(path1)
+
+def extract_prefix(file_name):
+    return int(file_name.split("-")[0])
+
+# 指定文件夹路径
+folder_path1 = './Test1'
+folder_path2 = './Test2'
+
+# 初始化一个空列表来存储文件名
+#用前缀进行排序
+file_list1 = sorted(getTxtName(folder_path1),key=extract_prefix)
+file_list2 = sorted(getTxtName(folder_path2),key=extract_prefix)
+##print(file_list1)
+
+##变量
+# 读取第一个文件的内容
+j = min(len(file_list2),len(file_list1))
+rangelist = list(range(0,j))
+
+for num in rangelist:
+
+    filename1 = file_list1[num]
+    filename2 = file_list2[num]
+    with open(os.path.join(folder_path1, filename1), 'r') as file1:
+        content1 = file1.read()
+
+    # 读取第二个文件的内容
+    with open(os.path.join(folder_path2, filename2), 'r') as file2:
+        content2 = file2.read()
+
+    # 定义属性名称
+    property_name = "provinces"
+
+    # 从文件1中提取属性定义
+    start_index1 = content1.find(f"{property_name} = {{")
+    end_index1 = content1.find("}", start_index1)
+    if start_index1 != -1 and end_index1 != -1:
+        property_definition1 = content1[start_index1:end_index1 + 1]
+    else:
+        property_definition1 = ""
+
+    # 从文件2中提取属性定义
+    start_index2 = content2.find(f"{property_name} = {{")
+    end_index2 = content2.find("}", start_index2)
+    if start_index2 != -1 and end_index2 != -1:
+        property_definition2 = content2[start_index2:end_index2 + 1]
+    else:
+        property_definition2 = ""
+
+    # 提取属性定义中的元素
+    def extract_elements(property_definition):
+        if property_definition:
+            elements = property_definition.split(" ")[1].strip("{}").split()
+            return set(elements)
+        return set()
+
+    # 提取两个属性定义中的元素
+    elements1 = extract_elements(property_definition1)
+    elements2 = extract_elements(property_definition2)
+
+    # 判断是否有交集
+    has_intersection = bool(elements1 & elements2)
+    #print(elements1)
+    #print(elements2)
+
+    if has_intersection:
+        print('true')
+    else:
+        print(f"false in'{filename1}'and'{filename2}'")
