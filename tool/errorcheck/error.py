f88a0bb6fb1ee71797ba312b3a9deba35724c67d
cupsmoke <42766513+cupsmoke@users.noreply.github.com>
2023-11-06 00:56:13 +0800

    Update error.py

--- tool/errorcheck/error.py
+++ tool/errorcheck/error.py
@@ -8,16 +8,20 @@ errorpath = os.path.join(docpath,'Paradox Interactive','Hearts of Iron IV','logs
 
 with open(errorpath,'r+',encoding='utf-8') as f:
     errorC = f.readlines()
+print('error共有',str(len(errorC)),"个")
 portraitError = []
+gfxError = []
 for line in errorC:
     line.lower()
+    if 'gfx/' in line and 'font' not in line:
+        gfxError.append(line)
     if 'portrait' in line:
         portraitError.append(line)
     elif 'gfx/leader' in line:
         portraitError.append(line)
-print(len(portraitError))
-for line in portraitError:
-    print(line)
+print(len(gfxError))
+for error in gfxError:
+    print(error)
 
 
 
