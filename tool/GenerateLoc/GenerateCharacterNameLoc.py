8ea265ba09635d2c1de5808fd6ddc666d11c5c55
Avant-gadre <49426761+Avant-gadre@users.noreply.github.com>
2023-11-16 17:10:09 +0800

    .

---
+++ tool/GenerateLoc/GenerateCharacterNameLoc.py
@@ -0,0 +1,45 @@
+import re
+import os
+import io
+import os.path
+
+inputpath = '..\common\\characters\\'
+outputpath = '.\\character_localisation\\'
+# inputfilename = input()
+inputfilename = 'BEY_Fate_Characters'
+if '.txt' not in inputfilename:
+    inputfilename = inputfilename + '.txt'
+if not os.path.exists(outputpath):
+    os.makedirs(outputpath)
+counter_leave = 0
+# 遇到{ +1  遇到} -1
+# counter_leave = 1 时 为id
+# counter_leave = 2 且name = 时为名字
+iputfullpath = inputpath + inputfilename
+if not os.path.exists(iputfullpath):
+    # 检查是否存在该文件
+    print('输入错了')
+    os.system("pause")
+else:
+    print('对了，这玩意存在')
+f = open(iputfullpath,'r+', encoding='utf-8')
+lines = f.readlines()
+f.close()
+
+namelist = []
+# print(lines[0])
+for line in lines:
+    if counter_leave == 1 and ' = {' in line :
+        # 这一行是id
+        id = re.sub(r'= {','',line).strip()
+        namelist.append(id)
+    counter_leave += line.count('{')
+    counter_leave -= line.count('}')
+
+
+f = open(outputpath + inputfilename, 'w', encoding='utf-8')
+for name in namelist:
+    name0 = name[4:].replace("_", " ").title()
+    full = name+":"+" "+"\""+name0+"\""
+    f.write(full+"\n")
+f.close()
