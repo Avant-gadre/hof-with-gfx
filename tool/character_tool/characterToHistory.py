9cdfd01903349cbe9c5c75acf24ea196fdd845e2
cupsmoke <42766513+cupsmoke@users.noreply.github.com>
2023-11-19 01:41:42 +0800

    just test

--- tool/character_tool/characterToHistory.py
+++ tool/character_tool/characterToHistory.py
@@ -11,6 +11,7 @@ class TagBlock:
 
 input_path1 = core.characterpath
 input_path2 = core.historyeffects
+outpath = os.path.join(core.rootpath, 'tool','countries-histroy')
 # print(input_path1)
 # print(input_path2)
 # output_path =
@@ -49,46 +50,50 @@ for char in charList:
     tag = char.strip().split('_')[0].upper()
     tagclist[taglist.index(tag)].append(char)
     # print(taglist.index(tag),tag,char)
-# for tag in taglist:
-#     for char in charList:
-# for tag in taglist:
-# for i in tagclist:
-#     print(i)
-# print(taglist)
+
 
 class historyBlock:
-    def __init__(self, id, context,ideas):
+    def __init__(self, tag, context,advisors,character):
         self.tag = tag
         self.context = context
-        self.context = ideas
+        self.advisors = advisors
+        self.character = character
 
 hisFiles = core.getTxtName(input_path2)
 for his in hisFiles:
     hispath = os.path.join(input_path2, his)
+    outfile = os.path.join(outpath,his)
+    # print(outfile)
+    tag = his[:3]
     try:
         with open(hispath,'r+',encoding='utf-8') as f2:
             # print(file)
             context2 = f2.readlines()
             count = 0
-            idea = []
+            advisors = []
             for line in context2:
-                if re.search('add_ideas.*= ?[a-zA-Z]'):
-                    idea.append(line.strip().split('=')[1].strip().split('#')[0])
-                    line = ''
-                if re.search('add_ideas.*=.*{',line):
-                    count = 1
-                    line = ''
-                elif count == 1 and '}' in line:
-                    count = 0
-                    line = ''
-                elif count == 1:
-                    idea.append(line.strip().split('#')[0].strip())
-                    line = ''
+                if re.search('activate_advisor.*= ?[a-zA-Z]',line):
+                    advisors.append(line)
+                    context2[context2.index(line)] = ''
+                    # print(line)
+                if 'recruit_character' in line:
+                    context2[context2.index(line)] = ''
+        characters = []
+        for subtag in taglist:
+            if tag == subtag:
+                characters = tagclist[taglist.index(subtag)]
+        tempblock = historyBlock(tag, context2, advisors,characters)
 
-
-
-
+        with open(outfile,'w',encoding='utf-8') as f3:
+            f3.writelines(tempblock.context)
+            f3.write('####character start\n')
+            for line in tempblock.character:
+                f3.write('recruit_character = ' + line + '\n')
+            f3.write('####character end\n')
+            if len(tempblock.advisors) >0 :
+                f3.write('####active advisor start\n')
+                f3.writelines(tempblock.advisors)
+                f3.write('####active advisor end\n')
     except:
-        print(his)
-    #
-    # 重写为其他-character-idea 顺序
+        print('character error')
+
