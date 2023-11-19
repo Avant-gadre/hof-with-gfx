d0e601aadac32202dea78a5fd7df622884afe434
cupsmoke <42766513+cupsmoke@users.noreply.github.com>
2023-11-19 21:52:11 +0800

    OVER

--- tool/character_tool/character_pdx_update.py
+++ tool/character_tool/character_pdx_update.py
@@ -17,7 +17,7 @@ counter_leave = 0
 # counter_leave = 2 且name = 时为名字
 # 分类方式
 catelist1 = ['country_leader = {','field_marshal = {','corps_commander = {','navy_leader = {']
-catelist2 = ['slot = army_chief','slot = navy_chief','slot = air_chief','slot = theorist','slot = high_command','slot = political_advisor','empty']
+catelist2 = ['slot = army_chief','slot = navy_chief','slot = air_chief','slot = theorist','slot = high_command','slot = political_advisor','empty_character']
 catelist0 = []
 catelist0.extend(catelist1)
 catelist0.extend(catelist2)
@@ -33,47 +33,46 @@ for txtname in txtlist:
     charactersCollection = []
     fullname = os.path.join(inputpath,txtname)
 
-    print(fullname)
+    # print(fullname)
     with open(fullname, 'r+', encoding='utf-8') as f:
         lines = f.readlines()
     counter_leave = 0
     # 分块
     for line in lines:
-        if counter_leave == 1 and '}' in line:
-            # 到结尾了
-            continue
-            # 这是一个char的结尾
         if counter_leave == 1 and ' = {' in line:
             num += 1
             # 这里是起始位置
             characterBlock = [line]
-        elif num > 0:
+        elif num > 0 and counter_leave > 1:
             characterBlock.append(line)
             if counter_leave == 2 and '\t}' in line:
                 # 这里是结束位置
                 charactersCollection.append(characterBlock)
-                print(characterBlock)
+                # print(characterBlock)
         counter_leave += line.count('{')
         counter_leave -= line.count('}')
-    # print(characterBlock)
+
     # 分类
     charactersReorder = []
-    for i in range(10):
+    for i in range(11):
         charactersReorder.append([])
     charactersCollectionSave = charactersCollection
     for character in charactersCollection:
-        cateid = 11
+        print(character[0])
+        catestr = ''.join(character)
+        cateid = 10
         for cate in catelist0:
-            for line in character:
-                if cate in line:
-                    # 找到对应的种类了！
-                    cateid = catelist0.index(cate)
-                    charactersReorder[cateid].extend(character)
-                    break
-            if cateid != 11 :
+            if cate in catestr:
+                # 找到对应的种类了！
+                cateid = catelist0.index(cate)
+                print(cateid)
+                charactersReorder[cateid].extend(character)
                 break
-    print(charactersReorder)
-    with open(outputpath + txtname, 'w+', encoding='utf-8') as f2:
+        if cateid == 10:
+            charactersReorder[10].extend(character)
+
+    # print(charactersReorder[10])
+    with open(outputpath + txtname, 'w', encoding='utf-8') as f2:
         f2.write('#' + txtname + '\n')
         for i in range(len(charactersReorder)):
             catename = catelist0[i]
