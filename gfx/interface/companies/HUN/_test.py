da7709cdbbb04937b3c2fd5a09fd18030dfbe0a8
Avant-gadre <49426761+Avant-gadre@users.noreply.github.com>
2023-11-23 20:09:33 +0800

    匈牙利 mio

---
+++ gfx/interface/companies/HUN/_test.py
@@ -0,0 +1,32 @@
+import os
+# 获取当前 Python 脚本的文件路径
+current_file_path = os.path.abspath(__file__)
+
+# 获取包含该文件的文件夹路径
+folder_path = os.path.dirname(current_file_path)
+
+# 列出当前文件夹所有文件
+files = os.listdir(folder_path)
+
+for file_name in files:
+    # 构建文件的完整路径
+    file_path = os.path.join(folder_path, file_name)
+    
+    # 检查是否是文件且是.png文件
+    if os.path.isfile(file_path) and file_name.lower().endswith('.png'):
+        # 1. 将文件名中的所有大写字母变为小写
+        new_file_name = file_name.lower()
+        
+        # 2. 开头有goal_的改名为focus_generic_开头
+        if new_file_name.startswith('idea_hun'):
+            new_file_name = 'hun' + new_file_name[len('idea_hun'):]
+
+        elif new_file_name.startswith('idea'):
+            new_file_name = 'hun' + new_file_name[len('idea'):]
+        
+        # # 4. 开头既没有focus_也没有goal_的加上focus_generic_
+        # elif not new_file_name.startswith('focus_') and not new_file_name.startswith('focus_generic_'):
+        #     new_file_name = 'focus_generic_' + new_file_name
+        
+        os.rename(file_path, os.path.join(folder_path, new_file_name))
+        print(f"文件 {file_name} 已重命名为 {new_file_name}")
