abaf3229afe79e3ebbbd68c2a700ec4b10f73a6d
Avant-gadre <49426761+Avant-gadre@users.noreply.github.com>
2023-11-25 13:23:17 +0800

    格林

--- tool/GenerateLoc/_workingplace/rename1.py
+++ tool/GenerateLoc/_workingplace/rename1.py
@@ -16,7 +16,7 @@ for file_name in files:
     if os.path.isfile(file_path) and file_name.lower().endswith('.png'):
         new_file_name = file_name
         
-        targetname = "EGY_"
+        targetname = "OTT_"
         targetname1 = "focus_"+targetname
 
         if new_file_name.startswith(targetname):
