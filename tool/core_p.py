1221b0ed569e79f292b1dbcfd5bf1c8c95484e07
cupsmoke <42766513+cupsmoke@users.noreply.github.com>
2023-11-25 17:33:27 +0800

    Create core_p.py

---
+++ tool/core_p.py
@@ -0,0 +1,9 @@
+import subprocess
+
+subprocess.run(['python', 'from png to gfx-event.py'])
+subprocess.run(['python', 'from png to gfx-company.py'])
+subprocess.run(['python', 'from png to gfx-focus.py'])
+subprocess.run(['python', 'from png to gfx-idea.py'])
+subprocess.run(['python', 'from png to gfx-news.py'])
+subprocess.run(['python', 'from png to gfx-portrait.py'])
+subprocess.run(['python', 'from png to gfx-advisor.py'])
