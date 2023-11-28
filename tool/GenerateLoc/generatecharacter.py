c2aca8545df7e3fe9e1d9edf491b5e5caf0d009e
Avant-gadre <49426761+Avant-gadre@users.noreply.github.com>
2023-11-28 13:30:12 +0800

    黄

--- tool/GenerateLoc/generatecharacter.py
+++ tool/GenerateLoc/generatecharacter.py
@@ -2,9 +2,11 @@ import os
 ##读取文件
 folder_path = ".//tool//GenerateLoc//_workingplace"
 
-output1 = os.join(folder_path,'generatecharacter.txt')
-output2 = os.join(folder_path,'generatecharacterloc.txt')
-output3 = os.join(folder_path,'generatecharacterhistory.txt')
+folder_path1 = ".//tool//GenerateLoc"
+
+output1 = os.path.join(folder_path1, 'generatecharacter.txt')
+output2 = os.path.join(folder_path1, 'generatecharacterloc.txt')
+output3 = os.path.join(folder_path1, 'generatecharacterhistory.txt')
 
 files = os.listdir(folder_path)
 # 要删除的特定前缀和后缀
@@ -80,9 +82,9 @@ characters_string = '''
 		}}
 		corps_commander = {{
 			traits = {{
-                china_shandong_clique_officer
+                china_shanxi_clique_officer
 			}}
-			skill = 2
+			skill = 3
 			attack_skill = 2
 			defense_skill = 2
 			planning_skill = 2
@@ -100,24 +102,27 @@ characters_string = '''
 	}}
 '''
 
-with open('generatecharacter.txt', 'a') as file:
+with open(output1 , 'a') as file:
     file.truncate(0)
     for name in files_set:
-        formatted_characters_string = characters_string.format(name, name, name, name, name)
+        name1 = name[:4]+name[4:].lower()
+        formatted_characters_string = characters_string.format(name1, name1, name, name, name1)
         file.write(formatted_characters_string+"\n")
     file.close()
 
-with open('generatecharacterloc.txt', 'a') as file1:
+with open(output2 , 'a') as file1:
     file1.truncate(0)
     for name in files_set:
+        name1 = name[:4]+name[4:].lower()
         name0 = name[4:].replace("_", " ").title()
-        full = name+":"+" "+"\""+name0+"\""
+        full = name1+":"+" "+"\""+name0+"\""
         file1.write(full+"\n")
     file1.close()
 
-with open('generatecharacterhistory.txt', 'a') as file2:
+with open(output3 , 'a') as file2:
     file2.truncate(0)
     for name in files_set:
+        name1 = name[:4]+name[4:].lower()
         desc = "recruit_character = "
-        file2.write(desc+name+"\n")
+        file2.write(desc+name1+"\n")
     file2.close()
