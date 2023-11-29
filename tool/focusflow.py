e33cea1388763b40b69f315ed64e8ebda1b3a232
Avant-gadre <49426761+Avant-gadre@users.noreply.github.com>
2023-11-29 20:44:46 +0800

    SPA

--- tool/focusflow.py
+++ tool/focusflow.py
@@ -6,7 +6,7 @@ import os.path
 inputpath = '.\\common\\national_focus\\'
 outputpath = '.\\tool\\focus_flow\\'
 # inputfilename = input()
-inputfilename = '01 SPF_Fate_focus'
+inputfilename = '03 SPA_Fate_focus'
 if '.txt' not in inputfilename:
     inputfilename = inputfilename + '.txt'
 if not os.path.exists(outputpath):
