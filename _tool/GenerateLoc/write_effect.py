1141095e54e94a6add319edfa90923527ab000c7
Avant-gadre <49426761+Avant-gadre@users.noreply.github.com>
2023-12-21 11:36:47 +0800

    工地

--- _tool/GenerateLoc/write_effect.py
+++ _tool/GenerateLoc/write_effect.py
@@ -1,5 +1,13 @@
-target = "CZE_sudeten_concessions"
+import os
 
+# 获取当前脚本文件的路径
+script_path = os.path.abspath(__file__)
+
+# 定义目标文件的相对路径（相对于脚本文件所在目录）
+output_path = "text2.txt"
+output_full_path = os.path.join(os.path.dirname(script_path), output_path)
+
+# statement模板
 statement_template = """
 else_if = {{
     limit = {{
@@ -12,6 +20,9 @@ else_if = {{
 }}
 """
 
-for i in range(1, 3):
-    current_statement = statement_template.format(i, i+1, target=target)
-    print(current_statement)
+target = "EGY_new_egyptian_army"
+
+with open(output_full_path, "w") as file:
+    for i in range(1, 12):
+        current_statement = statement_template.format(i, i + 1, target=target)
+        file.write(current_statement)
