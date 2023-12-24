4ccb647c13883d75c34b3bf84757005f9c6b20ea
Avant-gadre <49426761+Avant-gadre@users.noreply.github.com>
2023-12-24 22:42:54 +0800

    导入

--- Test/_tool/GenerateLoc/writeloc1.py
+++ Test/_tool/GenerateLoc/writeloc1.py
@@ -1,20 +1,27 @@
+import os
+
+# 定义文件路径
 path = ".//text.txt"
 path1 = ".//text1.txt"
 path2 = ".//text2.txt"
 path3 = ".//text3.txt"
 path4 = ".//text4.txt"
 
-target = "Generic_trait_"
+target = ""
+
+# 获取当前脚本文件的路径
+script_path = os.path.abspath(__file__)
+
+# 构建相对路径
+path1 = os.path.join(os.path.dirname(script_path), path1)
+path2 = os.path.join(os.path.dirname(script_path), path2)
 
 with open(path1, "r") as file:
     lines = file.readlines()
 
 with open(path2, "w") as file:
     for line in lines:
-        # parts = line.split(":")  # 以冒号分割行内容
-        # prefix = parts[0]  # 获取冒号前部分
-        prefix = line
-        prefix = prefix.strip()
+        prefix = line.strip()
         name0 = prefix[len(target) :].replace("_", " ").title().strip()
         full = " " + prefix + ":" + " " + '"' + name0 + '"'
         file.write(full + "\n")
